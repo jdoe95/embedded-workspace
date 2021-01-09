361835fdc19dc85097ff2ca5cfd5e7ac3b22d95c
John Yu <jyu@mestek.com>
2021-01-08 22:40:45 -0500

    TEST add platform/mem_copy

--- source/testing/platform/memfill.cc
+++ source/testing/platform/memfill.cc
@@ -13,6 +13,15 @@ TEST(MemZeropad, DeathTest)
 	EXPECT_DEATH(mem_zeropad(nullptr, 1, 1), TESTING_DEATH_STRING);
 }
 
+TEST(MemCopy, DeathTest)
+{
+	uint8_t to_dummy;
+	uint8_t from_dummy;
+	EXPECT_DEATH(mem_copy(nullptr, 0, nullptr, 0, 1), TESTING_DEATH_STRING);
+	EXPECT_DEATH(mem_copy(&to_dummy, sizeof(to_dummy), nullptr, 0, 1), TESTING_DEATH_STRING);
+	EXPECT_DEATH(mem_copy(nullptr, 0, &from_dummy, sizeof(from_dummy), 1), TESTING_DEATH_STRING);
+}
+
 TEST(MemZeropad, Test1)
 {
 	uint8_t test_vector[8] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88};
@@ -31,4 +40,37 @@ TEST(MemZeropad, Test2)
 
 	EXPECT_THAT(test_vector, testing::ElementsAre(
 			0x00, 0x00, 0x00, 0x00, 0x55, 0x66, 0x77, 0x88));
+}
+
+TEST(MemCopy, Test1)
+{
+	const uint8_t input[8] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88};
+	uint8_t output[8] = {0};
+
+	mem_copy(output, 0, input, 0, 8);
+
+	EXPECT_THAT(output, testing::ElementsAre(
+			0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88));
+}
+
+TEST(MemCopy, Test2)
+{
+	const uint8_t input[8] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88};
+	uint8_t output[8] = {0};
+
+	mem_copy(output, 0, input, 4, 4);
+
+	EXPECT_THAT(output, testing::ElementsAre(
+			0x55, 0x66, 0x77, 0x88, 0x00, 0x00, 0x00, 0x00));
+}
+
+TEST(MemCopy, Test3)
+{
+	const uint8_t input[8] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88};
+	uint8_t output[8] = {0};
+
+	mem_copy(output, 4, input, 0, 4);
+
+	EXPECT_THAT(output, testing::ElementsAre(
+			0x00, 0x00, 0x00, 0x00, 0x11, 0x22, 0x33, 0x44));
 }
