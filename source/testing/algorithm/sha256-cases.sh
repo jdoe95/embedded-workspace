3c9ce6be69080e0a6b02e4ddcefc5e99ce493b5b
John Yu <jyu@mestek.com>
2021-01-09 22:41:51 -0500

    TEST add algorithm/SHA256

---
+++ source/testing/algorithm/sha256-cases.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# This script generates 50 test cases for SHA256 unit tests
+# A SHA256 hash value is calculated by the script from a random challenge of random length
+# Both the random challenge and the hash are included in the test cases for the calculation to
+# compare against.
+# Created by John Yu
+
+# pick a random number between two values
+random_number(){
+	num1=$1
+	num2=$2
+	shuf -i "$1"-"$2" -n 1 
+}
+
+# generate random data and pack it with xxd
+random_data(){
+	length=$1
+	head --bytes="$length" /dev/urandom | xxd -p
+}
+
+# converts packed xxd data to C array content
+format_data(){
+	data=$1
+	echo "$data" | xxd -r -p | xxd -i
+}
+
+# counts the number of bytes in packed xxd data
+data_length(){
+	data=$1
+	echo "$data" | xxd -r -p | wc --bytes
+}
+
+# converts packed xxd data to hash value in packed xxd format
+hash_data(){
+	data=$1
+	echo "$data" | xxd -r -p | shasum -a256 | head --bytes=64
+}
+
+# prints test cases
+print_test(){
+	name=$1
+	data=$2
+	result=$3
+	len=$(data_length "$data")
+	echo -e "TEST(SHA256, $testname) {"
+	echo -e "\tstruct sha256 instance;"
+	echo -e "\tuint8_t result[SHA256_DIGEST_SIZE];"
+	echo -e "\tsha256_begin(&instance);"
+	echo -e "\tconst uint8_t test_data[$len] = {"
+	format_data "$data" | sed 's/^/\t\t/'
+	echo -e "\t};"
+	echo -e "\tsha256_update(&instance, test_data, $len);"
+	echo -e "\tsha256_finish(&instance, &result);"
+	echo -e "\tEXPECT_THAT(result, testing::ElementsAre("
+	format_data "$result" | sed 's/^/\t\t/'
+	echo -e "\t));"
+	echo -e "}"
+}
+
+gen_test() {
+	num_cases=$1
+	for i in $(seq 1 "$num_cases")
+	do
+		testname="Test$i"
+		len=$(random_number 1 300)
+		challenge=$(random_data "$len")
+		result=$(hash_data "$challenge")
+		print_test $testname "$challenge" "$result"
+	done
+}
+
+gen_test 50 > $1
+
---
+++ source/testing/algorithm/sha256.cc
@@ -0,0 +1,62 @@
+/*
+ * SHA256 Unit Tests
+ */
+#include <algorithm/sha256.h>
+#include <testing/death-string.h>
+#include <googletest/gmock.h>
+
+
+
+TEST(SHA256, DeathTest) {
+	struct sha256 dummy_instance;
+	uint8_t dummy_result[SHA256_DIGEST_SIZE];
+
+	EXPECT_DEATH(sha256_begin(nullptr),  TESTING_DEATH_STRING);
+
+	EXPECT_DEATH(sha256_update(nullptr, nullptr, (size_t) 1), TESTING_DEATH_STRING);
+	EXPECT_DEATH(sha256_update(&dummy_instance, nullptr, (size_t) 1), TESTING_DEATH_STRING);
+
+	EXPECT_DEATH(sha256_finish(nullptr, nullptr), TESTING_DEATH_STRING);
+	EXPECT_DEATH(sha256_finish(nullptr, dummy_result), TESTING_DEATH_STRING);
+	EXPECT_DEATH(sha256_finish(&dummy_instance, nullptr), TESTING_DEATH_STRING);
+}
+
+
+
+TEST(SHA256, EmptyVector) {
+	struct sha256 instance;
+	uint8_t result[SHA256_DIGEST_SIZE];
+
+	sha256_begin(&instance);
+	sha256_finish(&instance, &result);
+
+	EXPECT_THAT(result, testing::ElementsAre(
+			0xe3, 0xb0, 0xc4, 0x42, 0x98, 0xfc, 0x1c, 0x14,
+			0x9a, 0xfb, 0xf4, 0xc8, 0x99, 0x6f, 0xb9, 0x24,
+			0x27, 0xae, 0x41, 0xe4, 0x64, 0x9b, 0x93, 0x4c,
+			0xa4, 0x95, 0x99, 0x1b, 0x78, 0x52, 0xb8, 0x55));
+}
+
+
+
+TEST(SHA256, String) {
+	struct sha256 instance;
+	uint8_t result[SHA256_DIGEST_SIZE];
+
+	sha256_begin(&instance);
+	sha256_update(&instance, "The quick brown fox jumps over the lazy dog.", 44);
+	sha256_finish(&instance, &result);
+
+	EXPECT_THAT(result, testing::ElementsAre(
+			0xef, 0x53, 0x7f, 0x25, 0xc8, 0x95, 0xbf, 0xa7, 0x82, 0x52, 0x65, 0x29,
+			0xa9, 0xb6, 0x3d, 0x97, 0xaa, 0x63, 0x15, 0x64, 0xd5, 0xd7, 0x89, 0xc2,
+			0xb7, 0x65, 0x44, 0x8c, 0x86, 0x35, 0xfb, 0x6c));
+}
+
+
+
+/*
+ * Includes generated test cases
+ */
+#include "sha256.inc"
+
