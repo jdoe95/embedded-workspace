b62c7343d7654ed1face028f85343aa7c155c9ff
John Yu <jyu@mestek.com>
2021-01-20 18:38:07 -0500

    BUILD/sandbox add byteswap unit tests

--- recipe/sandbox/default.config.mk
+++ recipe/sandbox/default.config.mk
@@ -1,8 +1,36 @@
 MENU_VARIANT_SANDBOX=y
 MENU_GTEST=y
-MENU_GMOCK=y
+# MENU_GMOCK is not set
 
 #
 # GoogleTest Framework Settings
 #
 # end of GoogleTest Framework Settings
+
+MENU_TESTING=y
+
+#
+# Unit Tests
+#
+MENU_TEST_PLAT_BYTESWAP=y
+# end of Unit Tests
+
+#
+# Algorithm
+#
+
+#
+# Platform
+#
+# MENU_PLAT_NOCONTEND is not set
+# MENU_PLAT_USE_CONTEND_FAIL is not set
+MENU_PLAT_BYTESWAP=y
+# end of Platform
+
+#
+# Protocol
+#
+
+#
+# Utility
+#
